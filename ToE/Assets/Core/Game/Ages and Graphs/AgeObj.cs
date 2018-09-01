691ff4fcc5f758897632198dd7822184ae18af56
dv-k <DmitryTkachev@outlook.com>
2018-09-01 18:24:34 +0500

    Эксперемент с динамическими уровнями

--- ToE/Assets/Core/Game/Ages and Graphs/AgeObj.cs
+++ ToE/Assets/Core/Game/Ages and Graphs/AgeObj.cs
@@ -2,16 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using Assets.Core.Game.Ages_and_Graphs;
 
 public class AgeObj : MonoBehaviour
 {
     public Age Age;
     public List<GameObject> Levels;
+
     public SortLevels sortLevels;
     public Vector3 levelPos;
 
     void Start ()
     {
+
+
         sortLevels = new SortLevels();
         LoadLevels();
         sortLevels.Sorting();
@@ -19,16 +23,46 @@ public class AgeObj : MonoBehaviour
 	
 	void Update ()
     {
-        
-	}
+
+    }
+
+
+    
 
     void LoadLevels()
-    {     
-        for (int i = 0; i < Levels.Count; i++) 
+    {
+
+        /*   var r = GameObject.Find("Level");
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));
+           Levels.Add(GameObject.Find("Level"));*/
+
+        var level = new GameObject("Level");
+        level.AddComponent<SphereCollider>();
+        level.AddComponent<MeshRenderer>();
+        level.AddComponent<MeshFilter>();
+        level.AddComponent<LevelObj>();
+
+        Levels.Add(level);
+
+
+         for (int i = 0; i < Levels.Count; i++) 
         {
             Levels[i] = Instantiate(Levels[i]);
             Levels[i].transform.parent = this.transform;
             Levels[i].transform.position = sortLevels.randomFirstLevelPos(this.transform.localScale.x / 2);
+           // Debug.Log(Levels[i].transform.parent.name);
         }
         //sortLevels.StartSorting();
     }
