c5581fb0079574b6ea419863f638c1dbd5e7ce17
dv-k <DmitryTkachev@outlook.com>
2018-09-01 21:57:09 +0500

    2

--- ToE/Assets/Core/Game/Ages and Graphs/AgeObj.cs
+++ ToE/Assets/Core/Game/Ages and Graphs/AgeObj.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using Assets.Core.Game.Ages_and_Graphs;
+using UnityEngine.UI;
 
 public class AgeObj : MonoBehaviour
 {
     public Age Age;
     public List<GameObject> Levels;
-
+    public GameObject prefabLevel;
     public SortLevels sortLevels;
     public Vector3 levelPos;
+    public Canvas renderCanvas;
 
     void Start ()
     {
@@ -31,6 +33,7 @@ public class AgeObj : MonoBehaviour
 
     void LoadLevels()
     {
+        Resources.
 
         /*   var r = GameObject.Find("Level");
            Levels.Add(GameObject.Find("Level"));
@@ -48,20 +51,18 @@ public class AgeObj : MonoBehaviour
            Levels.Add(GameObject.Find("Level"));
            Levels.Add(GameObject.Find("Level"));*/
 
-        var level = new GameObject("Level");
-        level.AddComponent<SphereCollider>();
-        level.AddComponent<MeshRenderer>();
-        level.AddComponent<MeshFilter>();
-        level.AddComponent<LevelObj>();
 
-        Levels.Add(level);
-
-
-         for (int i = 0; i < Levels.Count; i++) 
+         for (int i = 0; i < 5; i++) 
         {
-            Levels[i] = Instantiate(Levels[i]);
-            Levels[i].transform.parent = this.transform;
-            Levels[i].transform.position = sortLevels.randomFirstLevelPos(this.transform.localScale.x / 2);
+            var r = Instantiate(this.prefabLevel);
+    
+       //     Text tx =  (Text)r.GetComponent(typeof(Text));
+         
+       
+      //      tx.text = i.ToString();
+            Levels.Add(r);
+            r.transform.parent = this.transform;
+            r.transform.position = sortLevels.randomFirstLevelPos(this.transform.localScale.x / 2);
            // Debug.Log(Levels[i].transform.parent.name);
         }
         //sortLevels.StartSorting();
--- ToE/Assets/Core/Game/Ages and Graphs/LevelObj.cs
+++ ToE/Assets/Core/Game/Ages and Graphs/LevelObj.cs
@@ -1,18 +1,24 @@
 ﻿using Assets.Core.Levels;
 using Assets.Core.Levels.Questions;
 using Assets.Core.Volutes;
+
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Assets.Core.Game.Ages_and_Graphs
 {
     public class LevelObj : MonoBehaviour
     {
         public ILevel level;
-        
 
+   
+        public Text text;
         void Start()
         {
-            
+        
+            text = Instantiate(this.text);
+            text.transform.parent = this.transform;
+            text.transform.position = new Vector3(10, 10);
         }
 
         void Update()
