76afe616d1ddf4e2672789f2581d4fd3bed5a412
Mr_Xoma <xomyakes@gmail.com>
2018-09-01 15:21:51 +0300

    Туц

---
+++ ToE/Assets/Core/Game/Ages and Graphs/WayDrawer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Core.Game.Ages_and_Graphs
+{
+    static class WayDrawer
+    {
+        public static void DrawWay(LevelObj parent, LevelObj child)
+            {
+            Vector3 first, second;
+            first = parent.transform.position;
+            second = child.transform.position;
+
+            }
+    }
+}
