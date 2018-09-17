a71d049abe24db968df0bdc1f07f2309c75e7589
dv-k <DmitryTkachev@outlook.com>
2018-09-17 18:40:41 +0500

    save

---
+++ ToE/Assets/Core/Game/Data/SaveGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Core.Game.Data
+{
+    public class SaveGame
+    {
+
+      //  public static void GetValue(IBase obj, [CallerMemberName] string name = "")
+//   }
+    }
+}
