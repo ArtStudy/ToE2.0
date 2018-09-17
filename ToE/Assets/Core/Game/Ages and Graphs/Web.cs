ab5680de9a4fff6f7b2e71e26da2969d4a65c662
Anton <xomyakes@gmail.com>
2018-09-17 15:58:10 +0300

    Some shit

---
+++ ToE/Assets/Core/Game/Ages and Graphs/Web.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Assets.Core.Game.Data.Level;
+using Assets.Core.LevelsStructureInterfaces;
+using UnityEngine;
+
+public class Web {
+    public Dictionary<int, List<ILevel>> groups;
+
+    //Инициализация сетки
+    public Web () {
+        groups = new Dictionary<int, List<ILevel>> ();
+        groups.Add (0, new List<ILevel> ());
+    }
+
+    public int LevelsCount = 0;
+
+    // Добавление нового уровня в сетку
+    public void AddLevel (ILevel level) {
+        int levelGroup = FindPlaceForLevel (level);
+        // Добавление уровня в сетку
+        if (groups.Count <= levelGroup)
+            groups.Add (levelGroup, new List<ILevel> ());
+        groups[levelGroup].Add (level);
+        LevelsCount++;
+        // Перестроение потомков уровня
+        MoveChildren (level, levelGroup);
+        //UnityEngine.Debug.Log ("Добавлен уровень в группу " + levelGroup);
+    }
+
+    // Сдвиг потомков добавленного уровня
+    private void MoveChildren (ILevel level, int parentGroup) {
+        for (int i = 0; i <= parentGroup; i++) {
+            foreach (var _level in groups[i]) {
+                for (int k = 0; k < _level.Parents.Count; k++) {
+                    if (_level.Parents[k] == level) {
+                        UnityEngine.Debug.Log ("Нашёл родителей для перестроение дерева");
+                        if (groups.Count <= parentGroup + 1)
+                            groups.Add (parentGroup + 1, new List<ILevel> ());
+                        groups[parentGroup + 1].Add (_level);
+                        groups[i].Remove (_level);
+                        MoveChildren (_level, parentGroup + 1);
+
+                    }
+                }
+            }
+        }
+
+    }
+
+    // Поиск группы добавляемого уровня
+    private int FindPlaceForLevel (ILevel level) {
+        int levelGroup = 0;
+        for (int i = 0; i < groups.Count; i++)
+            foreach (var _level in groups[i]) {
+                for (int k = 0; k < level.Parents.Count; k++) {
+                    if (_level == level.Parents[k])
+                        if (levelGroup <= i) {
+                            levelGroup = i + 1;
+                        }
+                }
+            }
+        return levelGroup;
+    }
+
+    //Поиск номера группы уровня
+    public int FindLevelGroup (ILevel level) {
+        int levelGroup = 0;
+        for (int i = 0; i < groups.Count; i++) {
+            foreach (var _level in groups[i])
+                if (_level == level) {
+                    levelGroup = i;
+                    return i;
+                }
+        }
+        UnityEngine.Debug.Log (levelGroup);
+        return levelGroup;
+    }
+
+}
--- ToE/Assets/Core/Game/Game.cs
+++ ToE/Assets/Core/Game/Game.cs
@@ -1,38 +1,36 @@
-﻿using Assets.Core.Game.Animations;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using UnityEngine;
-using Assets.Core.ToePac;
+using Assets.Core.Game.Animations;
 using Assets.Core.Game.Data;
-using Assets.Core.Levels;
 using Assets.Core.Game.Sorting;
+using Assets.Core.Levels;
+using Assets.Core.ToePac;
+using UnityEngine;
 
-public class Game : MonoBehaviour
-{
-
+public class Game : MonoBehaviour {
 
     public List<GameObject> Ages;
 
-
-    // Use this for initialization
-    void Start ()
-    { 
-        LoadAges();
+    public GameObject camera;
+    void Awake () {
+        //LoadGUI ();
+    }
+    void Start () {
+        LoadAges ();
     }
 
-    // Update is called once per frame
-    void Update ()
-    {
-        for (int i = 0; i < Ages.Count; i++) 
-            MenuAnimations.Rotation(Ages[i]);
+    void Update () {
+        //for (int i = 0; i < Ages.Count; i++) 
+        //MenuAnimations.Rotation(Ages[i]);
     }
 
-    void LoadAges()
-    {
-        for (int i = 0; i < Ages.Count; i++)
-        {
-            Ages[i] = Instantiate(Ages[i]);
+    void LoadAges () {
+        for (int i = 0; i < Ages.Count; i++) {
+            Ages[i] = Instantiate (Ages[i]);
         }
+    }
+    void LoadGUI () {
+        //camera = Instantiate (camera);
     }
 }
